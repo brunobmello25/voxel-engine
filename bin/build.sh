ac59e2999c9a1765e04d698a82148863a1adb44c
Bruno Mello <bruno.barros.mello@gmail.com>
2025-11-19 22:38:50 -0300

    basic window setup with sdl

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@ COMPILER="bear -- c++"
 mkdir -p ./target/
 
 pushd ./target/
-$COMPILER ../sauce/linux_main.cpp -o linux_app
+$COMPILER ../sauce/linux_main.cpp `pkg-config sdl3 --cflags --libs` -o linux_app
 popd
 
 echo "Build complete. Executable is located at ./target/linux_app"
--- sauce/linux_main.cpp
+++ sauce/linux_main.cpp
@@ -1,6 +1,37 @@
+#include <SDL3/SDL.h>
 #include <stdio.h>
 
+void processEvents(bool *running) {
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_EVENT_QUIT:
+      *running = false;
+      break;
+    }
+  }
+}
+
 int main() {
-  printf("hello world\n");
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    printf("SDL_Init failed: %s\n", SDL_GetError());
+    return 1;
+  };
+
+  SDL_Window *window =
+      SDL_CreateWindow("Voxel Engine", 1920 / 2, 1080 / 2,
+                       SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+  if (!window || !renderer) {
+    printf("SDL_CreateWindow or SDL_CreateRenderer failed: %s\n",
+           SDL_GetError());
+    return 1;
+  }
+
+  bool running = true;
+  while (running) {
+    processEvents(&running);
+  }
+
   return 0;
 }
