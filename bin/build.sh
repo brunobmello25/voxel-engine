efabba26d27739c5367e74e5bf1ec3bd2839c7cd
Bruno Mello <bruno.barros.mello@gmail.com>
2025-11-19 23:12:58 -0300

    update build script to use absolute paths on include

--- bin/build.sh
+++ bin/build.sh
@@ -2,8 +2,9 @@ COMPILER="bear -- c++"
 
 mkdir -p ./target/
 
+PROJECT_ROOT=$(pwd)
 pushd ./target/
-$COMPILER ../sauce/linux_main.cpp `pkg-config sdl3 --cflags --libs` -o linux_app
+$COMPILER ../sauce/linux_main.cpp -I${PROJECT_ROOT}/sauce/glad/include `pkg-config sdl3 --cflags --libs` -o linux_app
 popd
 
 echo "Build complete. Executable is located at ./target/linux_app"
